7d322e162a64a125478f44ddbc6a2f20ef4950f1
ShailyPande <connect2shailypandey@gmail.com>
2024-08-10 11:29:55 +0530

    Update Class_23.py
    Added comments to explain the codes

--- Class_23.py
+++ Class_23.py
@@ -5,15 +5,22 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 
+# Launch the below URL
 driver.get("https://jqueryui.com/droppable/")
+
+# Maximize the window
 driver.maximize_window()
 
+# Switch to frame where elments are present
 driver.switch_to.frame(0)
+
+# locate the drage and drop elements
 drag = driver.find_element(By.XPATH,"//div[@id='draggable']")
 drop = driver.find_element(By.XPATH,"//div[@id='droppable']")
 
 actions = ActionChains(driver)
 
+# drop the dragged elment
 actions.drag_and_drop(drag,drop).perform()
 
 time.sleep(5)
