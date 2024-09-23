f6b40afd38529eca61f3046e94b0efd4b7625c63
shaily Pandey <connect2shailypandey@gmail.com>
2024-09-23 23:32:50 +0530

    confest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import pytest
-from pages.login_page import LoginPage
 
 
 
@@ -24,19 +23,3 @@ def setup(request):
     driver.quit()
 
 
-# @pytest.fixture(scope="class")
-# def login(request, setup):
-#     """
-#     Fixture to log into the application.
-#     Assumes the setup_driver fixture is used to initialize the driver.
-#     """
-#     login_page = LoginPage(request.cls.driver)  # Pass the driver to the login page object
-#     login_page.enter_username("Admin")  # Replace with valid username
-#     login_page.enter_password("admin123")  # Replace with valid password
-#     login_page.click_login()
-#
-#     # Check if login was successful (optional)
-#     assert "dashboard" in request.cls.driver.current_url  # Assuming dashboard URL contains 'dashboard'
-#
-#     # Login is done, continue with the test
-#     yield request.cls.driver
